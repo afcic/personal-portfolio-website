156e1bf4d8bc370c56577cb11ed184d7ef3db573
Anja Ferjančič <anja.ferjancic@protonmail.com>
2021-11-29 18:25:19 +1300

    Addig my portfolio

---
+++ projects/projects.js
@@ -0,0 +1,51 @@
+const restapi = document.querySelector("#five");
+restapi.addEventListener("click", () => {
+  window
+    .open(
+      "https://afcic.github.io/FrontEndMentor_RESTCountriesAPI/",
+      "_blank",
+      "noopener noreferrer"
+    )
+    .focus();
+});
+
+const rockpaperscissors = document.querySelector("#six");
+rockpaperscissors.addEventListener("click", () => {
+  window
+    .open(
+      "https://afcic.github.io/FrontEndMentor_RockPaperScissors/",
+      "_blank",
+      "noopener noreferrer"
+    )
+    .focus();
+});
+
+const milestone = document.querySelector("#four");
+milestone.addEventListener("click", () => {
+  window.open("./milestoneStrength.html", "noopener noreferrer").focus();
+});
+
+const queerdir = document.querySelector("#three");
+queerdir.addEventListener("click", () => {
+  window.open("./queerdir.html", "noopener noreferrer").focus();
+});
+
+const cntstrck = document.querySelector("#two");
+cntstrck.addEventListener("click", () => {
+  window
+    .open(
+      "https://www.instagram.com/cntstrck/",
+      "_blank",
+      "noopener noreferrer"
+    )
+    .focus();
+});
+
+const olderProjects = document.querySelector("#one");
+olderProjects.addEventListener("click", () => {
+  window.open(
+    "./oldies_projects/AnjaFerjancic_portfolio.pdf",
+    "_blank",
+    "noopener noreferrer"
+  );
+});
---
+++ projects/queerdir.js
@@ -0,0 +1,105 @@
+function myFunction() {
+  const dots = document.getElementById("dots");
+  const moreText = document.getElementById("more");
+  const btnText = document.getElementById("myBtn");
+
+  if (dots.style.display === "none") {
+    dots.style.display = "inline";
+    btnText.innerHTML = "Read more";
+    moreText.style.display = "none";
+  } else {
+    dots.style.display = "none";
+    btnText.innerHTML = "Read less";
+    moreText.style.display = "inline";
+  }
+}
+function myFunction2() {
+  const dots = document.getElementById("dots2");
+  const moreText = document.getElementById("more2");
+  const btnText = document.getElementById("myBtn2");
+
+  if (dots.style.display === "none") {
+    dots.style.display = "inline";
+    btnText.innerHTML = "Read more";
+    moreText.style.display = "none";
+  } else {
+    dots.style.display = "none";
+    btnText.innerHTML = "Read less";
+    moreText.style.display = "inline";
+  }
+}
+function myFunction3() {
+  const dots = document.getElementById("dots3");
+  const moreText = document.getElementById("more3");
+  const btnText = document.getElementById("myBtn3");
+
+  if (dots.style.display === "none") {
+    dots.style.display = "inline";
+    btnText.innerHTML = "Read more";
+    moreText.style.display = "none";
+  } else {
+    dots.style.display = "none";
+    btnText.innerHTML = "Read less";
+    moreText.style.display = "inline";
+  }
+}
+function myFunction4() {
+  const dots = document.getElementById("dots4");
+  const moreText = document.getElementById("more4");
+  const btnText = document.getElementById("myBtn4");
+
+  if (dots.style.display === "none") {
+    dots.style.display = "inline";
+    btnText.innerHTML = "Read more";
+    moreText.style.display = "none";
+  } else {
+    dots.style.display = "none";
+    btnText.innerHTML = "Read less";
+    moreText.style.display = "inline";
+  }
+}
+function myFunction5() {
+  const dots = document.getElementById("dots5");
+  const moreText = document.getElementById("more5");
+  const btnText = document.getElementById("myBtn5");
+
+  if (dots.style.display === "none") {
+    dots.style.display = "inline";
+    btnText.innerHTML = "Read more";
+    moreText.style.display = "none";
+  } else {
+    dots.style.display = "none";
+    btnText.innerHTML = "Read less";
+    moreText.style.display = "inline";
+  }
+}
+function myFunction6() {
+  const dots = document.getElementById("dots6");
+  const moreText = document.getElementById("more6");
+  const btnText = document.getElementById("myBtn6");
+
+  if (dots.style.display === "none") {
+    dots.style.display = "inline";
+    btnText.innerHTML = "Read more";
+    moreText.style.display = "none";
+  } else {
+    dots.style.display = "none";
+    btnText.innerHTML = "Read less";
+    moreText.style.display = "inline";
+  }
+}
+function myFunction7() {
+  const dots = document.getElementById("dots7");
+  const moreText = document.getElementById("more7");
+  const btnText = document.getElementById("myBtn7");
+
+  if (dots.style.display === "none") {
+    dots.style.display = "inline";
+    btnText.innerHTML = "Read more";
+    moreText.style.display = "none";
+  } else {
+    dots.style.display = "none";
+    btnText.innerHTML = "Read less";
+    moreText.style.display = "inline";
+  }
+}
